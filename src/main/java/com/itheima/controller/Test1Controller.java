0bd0444e435a088b1a3dfcb957cb0263bd826835
hyx <hyx@itcast.cn>
2024-12-02 17:04:11 +0800

    test...

--- src/main/java/com/itheima/controller/Test1Controller.java
+++ src/main/java/com/itheima/controller/Test1Controller.java
@@ -3,5 +3,6 @@ package com.itheima.controller;
 public class Test1Controller {
     public static void main(String[] args) {
         System.out.println(1);
+        System.out.println(2);
     }
 }
